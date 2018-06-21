c578825d97c5abdc53121e32df85f674ec14d505
Joonwon Choi <joonwonc@mit.edu>
2018-06-21 00:52:21 -0400

    Existences of [Denotational] precondition and function for each [STransactional] case.

--- src/SerialRule.v
+++ src/SerialRule.v
@@ -309,6 +309,197 @@ Definition BCommutable (sys: System) (hst1 hst2: MHistory) :=
   behaviorOf sys hst2 ++ behaviorOf sys hst1 =
   behaviorOf sys hst1 ++ behaviorOf sys hst2.
 
+Lemma silent_denotational:
+  forall sys, Denotational sys (fun _ => True) id [RlblEmpty _].
+Proof.
+  intros.
+  hnf; split; hnf; intros.
+  - repeat econstructor.
+  - inv H; inv H3; inv H5; auto.
+Qed.
+
+Lemma ins_denotational:
+  forall sys eins,
+    eins <> nil ->
+    ValidMsgsExtIn sys eins ->
+    Denotational sys (fun _ => True)
+                 (fun st => {| bst_oss := bst_oss st;
+                               bst_orqs := bst_orqs st;
+                               bst_msgs := enqMsgs eins (bst_msgs st) |})
+                 [RlblIns eins].
+Proof.
+  intros.
+  hnf; split; hnf; intros.
+  - econstructor.
+    + econstructor.
+    + econstructor; eauto.
+      destruct st1; reflexivity.
+  - inv H1; inv H5; inv H7; auto.
+Qed.
+
+Lemma outs_denotational:
+  forall sys eouts,
+    eouts <> nil ->
+    ValidMsgsExtOut sys eouts ->
+    Denotational sys (fun st => Forall (FirstMPI (bst_msgs st)) eouts)
+                 (fun st => {| bst_oss := bst_oss st;
+                               bst_orqs := bst_orqs st;
+                               bst_msgs := deqMsgs (idsOf eouts) (bst_msgs st) |})
+                 [RlblOuts eouts].
+Proof.
+  intros.
+  hnf; split; hnf; intros.
+  - econstructor.
+    + econstructor.
+    + econstructor; eauto.
+      destruct st1; reflexivity.
+  - inv H1; inv H5; inv H7; auto.
+Qed.
+
+Inductive DenoteIntHst: Prec -> Trs -> MHistory -> Prop :=
+| DenoteNil: DenoteIntHst (fun _ => True) id nil
+| DenoteCons:
+    forall p f hst,
+      DenoteIntHst p f hst ->
+      forall rule ins outs,
+        DenoteIntHst
+          (fun st =>
+             p st /\
+             let oss := bst_oss (f st) in
+             let orqs := bst_orqs (f st) in
+             let msgs := bst_msgs (f st) in
+             Forall (FirstMPI msgs) ins /\
+             exists ost orq,
+               oss@[rule_oidx rule] = Some ost /\
+               orqs@[rule_oidx rule] = Some orq /\
+               rule_precond rule ost orq ins /\
+               snd (rule_trs rule ost orq ins) = outs
+          )
+          (fun st =>
+             let oss := bst_oss (f st) in
+             let orqs := bst_orqs (f st) in
+             let msgs := bst_msgs (f st) in
+             oss@[rule_oidx rule] >>=[st]
+                (fun ost =>
+                   orqs@[rule_oidx rule] >>=[st]
+                       (fun orq =>
+                          match rule_trs rule ost orq ins with
+                          | (nost, norq, _) =>
+                            {| bst_oss := oss +[rule_oidx rule <- nost];
+                               bst_orqs := orqs +[rule_oidx rule <- norq];
+                               bst_msgs := enqMsgs outs (deqMsgs (idsOf ins) msgs)
+                            |}
+                          end)))
+          (RlblInt rule ins outs :: hst).
+
+Lemma denoteIntHst_prec_trs_exist:
+  forall hst,
+    InternalHistory hst ->
+    exists p f, DenoteIntHst p f hst.
+Proof.
+  induction hst; simpl; intros.
+  - do 2 eexists; constructor.
+  - inv H.
+    specialize (IHhst H3).
+    destruct IHhst as [pp [pf ?]].
+    destruct a as [| |rule ins outs|]; try (elim H2; fail).
+    do 2 eexists.
+    econstructor; eauto.
+Qed.
+
+Definition WfLbl (sys: System) (lbl: MLabel) :=
+  match lbl with
+  | RlblEmpty _ => True
+  | RlblIns eins => eins <> nil /\ ValidMsgsExtIn sys eins
+  | RlblOuts eouts => eouts <> nil /\ ValidMsgsExtOut sys eouts
+  | RlblInt rule ins outs =>
+    In (rule_oidx rule) (sys_oinds sys) /\
+    ValidMsgsIn sys ins /\
+    idsOf ins = rule_minds rule /\
+    map msg_id (valsOf ins) = rule_msg_ids rule /\
+    In rule (sys_rules sys) /\
+    ValidMsgsOut sys outs /\
+    DisjList (idsOf ins) (idsOf outs)
+  end.
+
+Definition WfHistory (sys: System) (hst: MHistory) :=
+  Forall (WfLbl sys) hst.
+
+Lemma steps_wfHistory:
+  forall sys st1 hst st2,
+    steps step_m sys st1 hst st2 ->
+    WfHistory sys hst.
+Proof.
+  induction 1; simpl; intros; [constructor|].
+  constructor; auto.
+  clear H; inv H0; red; auto 7.
+Qed.
+
+Lemma denoteIntHst_denotationalL:
+  forall sys p f hst,
+    WfHistory sys hst ->
+    DenoteIntHst p f hst ->
+    DenotationalL sys p f hst.
+Proof.
+  induction 2; simpl; intros; [constructor|].
+  inv H; econstructor; [destruct H; eapply IHDenoteIntHst; eauto|].
+  dest.
+  remember (rule_trs rule x x0 ins) as nst.
+  destruct nst as [[noss norqs] nmsgs].
+  apply eq_sym in Heqnst.
+  simpl in H7; subst.
+  red in H3; dest.
+  econstructor; try reflexivity; try eassumption; simpl.
+  - destruct (f st1); reflexivity.
+  - rewrite H2, H5; simpl.
+    rewrite Heqnst; reflexivity.
+Qed.
+
+Lemma denoteIntHst_denotationalR:
+  forall sys p f hst,
+    DenoteIntHst p f hst ->
+    DenotationalR sys p f hst.
+Proof.
+  induction 1; simpl; intros;
+    [red; intros; inv H; auto|].
+
+  red; intros; inv H0.
+  specialize (IHDenoteIntHst _ _ H4); dest; subst.
+  inv H6; rewrite H21; simpl.
+  repeat split; auto.
+  - do 2 eexists; repeat split; try eassumption.
+    rewrite H16; reflexivity.
+  - rewrite H8, H9; simpl.
+    rewrite H16; reflexivity.
+Qed.
+
+Lemma denoteIntHst_denotational:
+  forall sys p f hst,
+    WfHistory sys hst ->
+    DenoteIntHst p f hst ->
+    Denotational sys p f hst.
+Proof.
+  intros; split.
+  - apply denoteIntHst_denotationalL; auto.
+  - apply denoteIntHst_denotationalR; auto.
+Qed.
+
+Lemma atomic_denotational:
+  forall sys inits ins hst outs eouts,
+    WfHistory sys hst ->
+    Atomic msg_dec inits ins hst outs eouts ->
+    exists p f,
+      Denotational sys p f hst /\
+      DenoteIntHst p f hst.
+Proof.
+  intros.
+  apply atomic_internal_history in H0.
+  apply denoteIntHst_prec_trs_exist in H0.
+  destruct H0 as [p [f ?]].
+  pose proof (denoteIntHst_denotational H H0).
+  eauto.
+Qed.
+
 Lemma nonconflicting_reduced:
   forall sys hst1 hst2 p1 p2 f1 f2,
     BCommutable sys hst1 hst2 ->
@@ -380,84 +571,9 @@ Proof.
   eapply discontinuous_trs_type_bcommutable; eauto.
 Qed.
 
-Lemma silent_denotational:
-  forall sys, Denotational sys (fun _ => True) id [RlblEmpty _].
-Proof.
-  intros.
-  hnf; split; hnf; intros.
-  - repeat econstructor.
-  - inv H; inv H3; inv H5; auto.
-Qed.
-
-Lemma ins_denotational:
-  forall sys eins,
-    eins <> nil ->
-    ValidMsgsExtIn sys eins ->
-    Denotational sys (fun _ => True)
-                 (fun st => {| bst_oss := bst_oss st;
-                               bst_orqs := bst_orqs st;
-                               bst_msgs := enqMsgs eins (bst_msgs st) |})
-                 [RlblIns eins].
-Proof.
-  intros.
-  hnf; split; hnf; intros.
-  - econstructor.
-    + econstructor.
-    + econstructor; eauto.
-      destruct st1; reflexivity.
-  - inv H1; inv H5; inv H7; auto.
-Qed.
-
-Lemma outs_denotational:
-  forall sys eouts,
-    eouts <> nil ->
-    ValidMsgsExtOut sys eouts ->
-    Denotational sys (fun st => Forall (FirstMPI (bst_msgs st)) eouts)
-                 (fun st => {| bst_oss := bst_oss st;
-                               bst_orqs := bst_orqs st;
-                               bst_msgs := deqMsgs (idsOf eouts) (bst_msgs st) |})
-                 [RlblOuts eouts].
-Proof.
-  intros.
-  hnf; split; hnf; intros.
-  - econstructor.
-    + econstructor.
-    + econstructor; eauto.
-      destruct st1; reflexivity.
-  - inv H1; inv H5; inv H7; auto.
-Qed.
-
-(* Definition DiscontinuousA (hst1 hst2: MHistory) := *)
-(*   forall inits1 ins1 outs1 eouts1 inits2 ins2 outs2 eouts2, *)
-(*     Atomic msg_dec inits1 ins1 hst1 outs1 eouts1 -> *)
-(*     Atomic msg_dec inits2 ins2 hst2 outs2 eouts2 -> *)
-(*     DisjList (idsOf ins1) (idsOf ins2) /\ *)
-(*     DisjList eouts1 inits2 /\ *)
-(*     DisjList (idsOf outs1) (idsOf outs2). *)
-
-(* Definition DiscontinuousIns (hst1 hst2: MHistory) := *)
-(*   forall eins1 inits2 ins2 outs2 eouts2, *)
-(*     hst1 = [RlblIns eins1] -> *)
-(*     Atomic msg_dec inits2 ins2 hst2 outs2 eouts2 -> *)
-(*     DisjList eins1 ins2 /\ *)
-(*     DisjList (idsOf eins1) (idsOf outs2). *)
-
-(* Definition DiscontinuousOuts (hst1 hst2: MHistory) := *)
-(*   forall inits1 ins1 outs1 eouts1 eouts2, *)
-(*     Atomic msg_dec inits1 ins1 hst1 outs1 eouts1 -> *)
-(*     hst2 = [RlblOuts eouts2] -> *)
-(*     DisjList eouts2 outs1 /\ *)
-(*     DisjList (idsOf eouts2) (idsOf ins1). *)
-
-(* Definition Discontinuous (hst1 hst2: MHistory) := *)
-(*   DiscontinuousTrsType (trsTypeOf hst1) (trsTypeOf hst2) /\ *)
-(*   DiscontinuousA hst1 hst2 /\ *)
-(*   DiscontinuousIns hst1 hst2 /\ *)
-(*   DiscontinuousOuts hst1 hst2. *)
-
-(** TODO: need one more condition about "discontinuity" *)
-Corollary discontinuous_trs_type_nonconflicting:
+Lemma discontinuous_trs_type_nonconflicting:
   forall sys hst1 hst2,
+    WfHistory sys hst1 -> WfHistory sys hst2 ->
     STransactional msg_dec hst1 ->
     STransactional msg_dec hst2 ->
     DiscontinuousTrsType (trsTypeOf hst1) (trsTypeOf hst2) ->
@@ -466,7 +582,97 @@ Corollary discontinuous_trs_type_nonconflicting:
       Denotational sys p2 f2 hst2 /\
       Nonconflicting p1 p2 f1 f2.
 Proof.
-Abort.
+  intros; inv H1.
+
+  - inv H2.
+    + do 4 eexists.
+      split; [|split];
+        [apply silent_denotational|apply silent_denotational|].
+      hnf; auto.
+    + do 4 eexists.
+      inv H0; destruct H4; clear H5.
+      split; [|split];
+        [apply silent_denotational|apply ins_denotational; auto|].
+      hnf; auto.
+    + do 4 eexists.
+      inv H0; destruct H4; clear H5.
+      split; [|split];
+        [apply silent_denotational|apply outs_denotational; auto|].
+      hnf; auto.
+    + eapply atomic_denotational in H1; eauto.
+      destruct H1 as [p2 [f2 [? ?]]].
+      do 4 eexists.
+      split; [|split]; [apply silent_denotational|eassumption|].
+      hnf; auto.
+
+  - inv H2; try (elim H3; fail).
+    + do 4 eexists.
+      inv H; destruct H4; clear H5.
+      split; [|split];
+        [apply ins_denotational; auto|apply silent_denotational|].
+      hnf; auto.
+    + pose proof (atomic_denotational H0 H1).
+      destruct H2 as [p2 [f2 [? ?]]].
+      inv H; inv H7; clear H8.
+      do 4 eexists.
+      split; [|split]; [apply ins_denotational; auto|eassumption|].
+      (** TODO: prove a lemma like:
+       * Atomic msg_dec inits ins hst2 outs eouts ->
+       * DenoteIntHst p f hst2 ->
+       * DisjList eins ins ->
+       * DisjList (idsOf eins) (idsOf outs) ->
+       * Nonconflicting (fun _ : MState => True) p
+       *   (fun st : MState =>
+       *     {| bst_oss := bst_oss st;
+       *        bst_orqs := bst_orqs st;
+       *        bst_msgs := enqMsgs eins (bst_msgs st) |}) f.
+       *)
+      admit. (* similar to [msg_ins_reduced_2] *)
+
+  - inv H2; try (elim H3; fail).
+    + do 4 eexists.
+      inv H; destruct H4; clear H5.
+      split; [|split];
+        [apply outs_denotational; auto|apply silent_denotational|].
+      hnf; auto.
+    + pose proof (atomic_denotational H0 H1).
+      destruct H2 as [p2 [f2 [? ?]]].
+      inv H; inv H7; clear H8.
+      do 4 eexists.
+      split; [|split]; [apply outs_denotational; auto|eassumption|].
+      admit. (* similar to [msg_outs_reduced_1] *)
+
+  - inv H2.
+    + eapply atomic_denotational in H4; eauto.
+      destruct H4 as [p1 [f1 [? ?]]].
+      do 4 eexists.
+      split; [|split]; [eassumption|apply silent_denotational|].
+      hnf; auto.
+    + pose proof (atomic_denotational H H4).
+      destruct H1 as [p1 [f1 [? ?]]].
+      inv H0; inv H7; clear H8.
+      do 4 eexists.
+      split; [|split]; [eassumption|apply ins_denotational; auto|].
+      admit. (* similar to [msg_ins_reduced_1] *)
+    + pose proof (atomic_denotational H H4).
+      destruct H1 as [p1 [f1 [? ?]]].
+      inv H0; inv H7; clear H8.
+      do 4 eexists.
+      split; [|split]; [eassumption|apply outs_denotational; auto|].
+      rename eouts0 into mouts.
+      (** TODO: prove a lemma similar to above, requiring:
+       * DisjList (idsOf mouts) (idsOf ins) /\
+       * DisjList mouts outs *)
+      admit. (* similar to [msg_outs_reduced_2] *)
+
+    + pose proof (atomic_denotational H H4).
+      destruct H2 as [p1 [f1 [? ?]]].
+      pose proof (atomic_denotational H0 H1).
+      destruct H6 as [p2 [f2 [? ?]]].
+      exists p1, p2, f1, f2.
+      do 2 (split; auto).
+      admit.
+Admitted.
 
 Lemma interleaved_min:
   forall hsts, Interleaved hsts -> MinInterleaved hsts.
