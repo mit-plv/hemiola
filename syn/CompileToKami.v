b28c1825d12c296de6e30c7589742e1fa9aec698
Joonwon Choi <joonwonc@mit.edu>
2020-03-01 17:24:30 -0500

    wip compilation of Hemiola rules

--- syn/CompileToKami.v
+++ syn/CompileToKami.v
@@ -1,6 +1,6 @@
 Require Import Hemiola.Common Hemiola.Syntax HemiolaDeep.
 
-Require Import Kami.
+Require Import Kami.Lib.Struct Kami.
 
 Set Implicit Arguments.
 
@@ -8,18 +8,105 @@ Import MonadNotations.
 Module H := Hemiola.Syntax.
 Module K := Kami.Syntax.
 
-Section I.
+Section Compile.
   Context `{DecValue} `{oifc: OStateIfc}.
 
   Definition compile_OState_init: list K.RegInitT :=
     nil. (** TODO *)
 
+  Section PreEval.
+
+    Definition HOIdx := Bit 8.
+    Definition HMIdx := Bit 4.
+    Definition HValue := Bit 32.
+
+    Definition HMsg :=
+      STRUCT { "id" :: HMIdx;
+               "type" :: Bool;
+               "value" :: HValue }.
+
+    Definition UpLock :=
+      STRUCT { "ul_valid" :: Bool;
+               "ul_rsb" :: Bool;
+               "ul_msg" :: Struct HMsg;
+               "ul_rssFrom" :: Array HOIdx 8;
+               "ul_rsbTo" :: HOIdx }.
+
+    Definition DownLock :=
+      STRUCT { "dl_valid" :: Bool;
+               "dl_rsb" :: Bool;
+               "dl_msg" :: Struct HMsg;
+               "dl_rssFrom" :: Array HOIdx 8;
+               "dl_rsbTo" :: HOIdx }.
+
+    Context {var: K.Kind -> Type}.
+    Variables (msgIn: var (Struct HMsg))
+              (ul: var (Struct UpLock))
+              (dl: var (Struct DownLock)).
+
+    Definition compile_Rule_rqrs_prec (rrp: HOPrecR)
+               (cont: K.ActionT var K.Void): K.ActionT var K.Void :=
+      (match rrp with
+       | HRqAccepting => (Assert (!(#msgIn!HMsg@."type")); cont)
+       | HRsAccepting => (Assert (#msgIn!HMsg@."type"); cont)
+       | HUpLockFree => (Assert (!(#ul!UpLock@."ul_valid")); cont)
+       | HDownLockFree => (Assert (!(#dl!DownLock@."dl_valid")); cont)
+       | HUpLockMsgId mty mid =>
+         (Assert (#ul!UpLock@."ul_valid");
+         Assert (#ul!UpLock@."ul_rsb");
+         Assert (#ul!UpLock@."ul_msg"!HMsg@."type" == Const _ (ConstBool mty));
+         (* Assert (#ul!UpLock@."ul_msg"!HMsg@."id" == $mid); *) (** FIXME *)
+         cont)
+       | HUpLockMsg =>
+         (Assert (#ul!UpLock@."ul_valid"); Assert (#ul!UpLock@."ul_rsb"); cont)
+       | HUpLockIdxBack =>
+         (Assert (#ul!UpLock@."ul_valid"); Assert (#ul!UpLock@."ul_rsb"); cont)
+       | HUpLockBackNone =>
+         (Assert (#ul!UpLock@."ul_valid"); Assert (!(#ul!UpLock@."ul_rsb")); cont)
+       | HDownLockMsgId mty mid =>
+         (Assert (#dl!DownLock@."dl_valid");
+         Assert (#dl!DownLock@."dl_rsb");
+         Assert (#dl!DownLock@."dl_msg"!HMsg@."type" == Const _ (ConstBool mty));
+         (* Assert (#dl!DownLock@."dl_msg"!HMsg@."id" == $mid); *) (** FIXME *)
+         cont)
+       | HDownLockMsg =>
+         (Assert (#dl!DownLock@."dl_valid"); Assert (#dl!DownLock@."dl_rsb"); cont)
+       | HDownLockIdxBack =>
+         (Assert (#dl!DownLock@."dl_valid"); Assert (#dl!DownLock@."dl_rsb"); cont)
+       | HMsgsFrom _ => cont (** TODO: how to deal with multiple input messages? *)
+       | HMsgIdsFrom _ => cont (** TODO: ditto *)
+       | HMsgIdFromEach _ => cont (** TODO: ditto *)
+       | HMsgsFromORq _ => cont (** TODO: ditto *)
+       end)%kami_action.
+    
+    Definition compile_Rule_prop_prec (pp: HOPrecP)
+               (cont: K.ActionT var K.Void): K.ActionT var K.Void :=
+      cont. (** TODO *)
+
+    Fixpoint compile_Rule_prec (rp: HOPrec)
+             (cont: K.ActionT var K.Void): K.ActionT var K.Void :=
+      match rp with
+      | HOPrecAnd prec1 prec2 =>
+        let crule1 := compile_Rule_prec prec1 cont in
+        compile_Rule_prec prec2 crule1
+      | HOPrecRqRs rrprec => compile_Rule_rqrs_prec rrprec cont
+      | HOPrecProp pprec => compile_Rule_prop_prec pprec cont
+      end.
+
+    Definition compile_Rule_trs (rtrs: HOTrs): K.Action K.Void :=
+      cheat _.
+
+  End PreEval.
+
   Definition compile_Rule (rule: {sr: H.Rule & HRule sr}):
-    Struct.Attribute (K.Action K.Void) :=
-    cheat _. (** TODO *)
+    Attribute (K.Action K.Void) :=
+    (* let ctrs := compile_Rule_trs (hrule_trs (projT2 rule)) in *)
+    (* let crule := compile_Rule_prec (hrule_precond (projT2 rule)) ctrs in *)
+    (* {| attrName := ""; attrType := crule |}. *)
+    cheat _. (** FIXME *)
   
   Definition compile_Rules (rules: list {sr: H.Rule & HRule sr}):
-    list (Struct.Attribute (K.Action K.Void)) :=
+    list (Attribute (K.Action K.Void)) :=
     map compile_Rule rules.
   
   Definition compile_Object (obj: HObject): option K.Modules :=
@@ -39,5 +126,5 @@ Section I.
   Definition compile_System (sys: HSystem): option Kami.Syntax.Modules :=
     compile_Objects sys.(hsys_objs).
 
-End I.
+End Compile.
 
--- syn/HemiolaDeep.v
+++ syn/HemiolaDeep.v
@@ -1,154 +1,177 @@
-Require Import Hemiola.Common Hemiola.Index Hemiola.HVector Hemiola.Syntax.
+Require Import Hemiola.Common Hemiola.Index Hemiola.HVector.
+Require Import Hemiola.Topology Hemiola.Syntax.
 Require Import Hemiola.RqRsLangEx.
         
 Set Implicit Arguments.
 
+Import MonadNotations.
+
+(* {| ost_ty := [nat:Type; bool:Type; MESI:Type; DirT:Type]%vector |}. *)
+
+(* Inductive HTypes {t} (var: Type -> t): forall {sz}, Vector.t Type sz -> Type := *)
+(* | HTyNil: HTypes var (Vector.nil _) *)
+(* | HTyCons: *)
+(*     forall {sz} {tys: Vector.t Type sz}, *)
+(*       HTypes var tys -> *)
+(*       forall {ty}, *)
+(*         HType var ty -> *)
+(*         HTypes var (ty :: tys). *)
+
+(* Record HOStateIfc (oifc: OStateIfc) := *)
+(*   { host_ty: HTypes (@ost_ty oifc); *)
+(*     host_init: *)
+(*   }. *)
+
 Section Reify.
   Context `{DecValue} `{oifc: OStateIfc}.
 
-  Inductive HValue: forall {ty}, ty -> Type :=
-  | HConst: forall {ty} (t: ty), HValue t
-  | HOstVal: forall (ost: OState) idx, HValue (ost#[idx]%hvec).
+  (** Reified syntax *)
+  
+  (* -- basis *)
+  
+  Inductive HValue: Type -> Type :=
+  | HConst: forall {ty} (c: ty), HValue ty
+  | HOstVal: forall idx, HValue (Vector.nth (@ost_ty oifc) idx).
 
-  Inductive HProp: Prop -> Type :=
-  | HAnd: forall P1 P2, HProp P1 -> HProp P2 -> HProp (P1 /\ P2)
-  | HOr: forall P1 P2, HProp P1 -> HProp P2 -> HProp (P1 \/ P2)
-  | HEq: forall t (e1 e2: t), HValue e1 -> HValue e2 -> HProp (e1 = e2)
-  | HNe: forall t (e1 e2: t), HValue e1 -> HValue e2 -> HProp (e1 <> e2)
-  | HLt: forall (e1 e2: nat), HValue e1 -> HValue e2 -> HProp (e1 < e2)
-  | HLe: forall (e1 e2: nat), HValue e1 -> HValue e2 -> HProp (e1 <= e2)
-  | HGt: forall (e1 e2: nat), HValue e1 -> HValue e2 -> HProp (e1 > e2)
-  | HGe: forall (e1 e2: nat), HValue e1 -> HValue e2 -> HProp (e1 >= e2).
+  (* -- for precondition *)
+  
+  Inductive HOPrecP: Type :=
+  | HAnd: HOPrecP -> HOPrecP -> HOPrecP
+  | HOr: HOPrecP -> HOPrecP -> HOPrecP
+  | HEq: HValue nat -> HValue nat -> HOPrecP
+  | HNe: HValue nat -> HValue nat -> HOPrecP
+  | HLt: HValue nat -> HValue nat -> HOPrecP
+  | HLe: HValue nat -> HValue nat -> HOPrecP
+  | HGt: HValue nat -> HValue nat -> HOPrecP
+  | HGe: HValue nat -> HValue nat -> HOPrecP.
 
-  Inductive HRqRsOPrec: OPrec -> Type :=
-  | HRqAccepting: HRqRsOPrec RqAccepting
-  | HRsAccepting: HRqRsOPrec RsAccepting
-  | HUpLockFree: HRqRsOPrec UpLockFree
-  | HDownLockFree: HRqRsOPrec DownLockFree
-  | HFirstMsg: HRqRsOPrec FirstMsg
-  | HUpLockMsgId: forall mty mid, HRqRsOPrec (UpLockMsgId mty mid)
-  | HUpLockMsg: HRqRsOPrec UpLockMsg
-  | HUpLockIdxBack: HRqRsOPrec UpLockIdxBack
-  | HUpLockBackNone: HRqRsOPrec UpLockBackNone
-  | HDownLockMsgId: forall mty mid, HRqRsOPrec (DownLockMsgId mty mid)
-  | HDownLockMsg: HRqRsOPrec DownLockMsg
-  | HDownLockIdxBack: HRqRsOPrec DownLockIdxBack
-  | HMsgsFrom: forall froms, HRqRsOPrec (MsgsFrom froms)
-  | HMsgIdsFrom: forall msgIds, HRqRsOPrec (MsgIdsFrom msgIds)
-  | HMsgIdFromEach: forall msgId, HRqRsOPrec (MsgIdFromEach msgId)
-  | HMsgsFromORq: forall rqty, HRqRsOPrec (MsgsFromORq rqty).
+  Inductive HOPrecR: Type :=
+  | HRqAccepting: HOPrecR
+  | HRsAccepting: HOPrecR
+  | HUpLockFree: HOPrecR
+  | HDownLockFree: HOPrecR
+  (* | HFirstMsg: HOPrecR *)
+  | HUpLockMsgId (mty: bool) (mid: IdxT): HOPrecR
+  | HUpLockMsg: HOPrecR
+  | HUpLockIdxBack: HOPrecR
+  | HUpLockBackNone: HOPrecR
+  | HDownLockMsgId (mty: bool) (mid: IdxT): HOPrecR
+  | HDownLockMsg: HOPrecR
+  | HDownLockIdxBack: HOPrecR
+  | HMsgsFrom (froms: list IdxT): HOPrecR
+  | HMsgIdsFrom (msgIds: list IdxT): HOPrecR
+  | HMsgIdFromEach (msgId: IdxT): HOPrecR
+  | HMsgsFromORq (rqty: IdxT): HOPrecR.
 
-  Inductive HOPrec: OPrec -> Type :=
-  | HOPrecAnd: forall {pr1 pr2}, HOPrec pr1 -> HOPrec pr2 -> HOPrec (OPrecAnd pr1 pr2)
-  | HOPrecT: forall {pr}, HRqRsOPrec pr -> HOPrec pr
-  | HOPrecP: forall {pr}, (forall ost orq mins, HProp (pr ost orq mins)) -> HOPrec pr.
+  Inductive HOPrec: Type :=
+  | HOPrecAnd: HOPrec -> HOPrec -> HOPrec
+  | HOPrecRqRs: HOPrecR -> HOPrec
+  | HOPrecProp: HOPrecP -> HOPrec.
 
-  (** TODO: may need this [bvrep] *)
-  (* Variable (bvrep: forall {V}, option V -> Type). *)
-  Inductive HMonad {A}: option A -> Type :=
-  | HBind:
-      forall {V} (ov: option V),
-      (* bvrep ov -> *)
-      forall {cont}, (forall v, HMonad (cont v)) -> HMonad (bind ov cont)
-  | HRet: forall v, HMonad (Some v).
+  (* -- for transition *)
 
-  Section ReifyTrs.
+  Inductive HBindValue: Type -> Type :=
+  | HGetFirstMsg: HBindValue Msg.
+
+  Inductive HMonad: Type :=
+  | HBind: forall {bt} (bv: HBindValue bt) (cont: bt -> HMonad), HMonad
+  | HRet: forall (rst: StateM), HMonad.
+
+  Inductive HStateMTrs: Type :=
+  | HMTrs: HMonad -> HStateMTrs.
+
+  Inductive HOTrs: Type :=
+  | HTrsMTrs: HStateMTrs -> HOTrs.
+
+  (** Interpretation *)
+  
+  Section Interp.
     Variables (ost: OState) (orq: ORq Msg) (mins: list (Id Msg)).
 
-    Inductive StateMTrsR: StateMTrs -> Type :=
-    | HStateMTrs:
-        forall mtrs,
-          (forall stm, HMonad (mtrs stm)) ->
-          StateMTrsR mtrs.
+    Definition interpValue {ty} (hv: HValue ty): ty :=
+      match hv with
+      | HConst c => c
+      | HOstVal idx => (ost#[idx])%hvec
+      end.
 
-    Inductive OTrsR: OTrs -> Type :=
-    | HTrsMTrs:
-        forall mtrs,
-          StateMTrsR mtrs ->
-          OTrsR (TrsMTrs mtrs).
+    Fixpoint interpOPrecP (p: HOPrecP): Prop :=
+      match p with
+      | HAnd p1 p2 => interpOPrecP p1 /\ interpOPrecP p2
+      | HOr p1 p2 => interpOPrecP p1 \/ interpOPrecP p2
+      | HEq v1 v2 => interpValue v1 = interpValue v2
+      | HNe v1 v2 => interpValue v1 <> interpValue v2
+      | HLt v1 v2 => interpValue v1 < interpValue v2
+      | HLe v1 v2 => interpValue v1 <= interpValue v2
+      | HGt v1 v2 => interpValue v1 > interpValue v2
+      | HGe v1 v2 => interpValue v1 >= interpValue v2
+      end.
 
-  End ReifyTrs.
+    Definition interpOPrecR (p: HOPrecR): Prop :=
+      match p with
+      | HRqAccepting => RqAccepting ost orq mins
+      | HRsAccepting => RsAccepting ost orq mins
+      | HUpLockFree => UpLockFree ost orq mins
+      | HDownLockFree => DownLockFree ost orq mins
+      (* | HFirstMsg => FirstMsg ost orq mins *)
+      | HUpLockMsgId mty mid => UpLockMsgId mty mid ost orq mins
+      | HUpLockMsg => UpLockMsg ost orq mins
+      | HUpLockIdxBack => UpLockIdxBack ost orq mins
+      | HUpLockBackNone => UpLockBackNone ost orq mins
+      | HDownLockMsgId mty mid => DownLockMsgId mty mid ost orq mins
+      | HDownLockMsg => DownLockMsg ost orq mins
+      | HDownLockIdxBack => DownLockIdxBack ost orq mins
+      | HMsgsFrom froms => MsgsFrom froms ost orq mins
+      | HMsgIdsFrom msgIds => MsgIdsFrom msgIds ost orq mins
+      | HMsgIdFromEach msgId => MsgIdFromEach msgId ost orq mins
+      | HMsgsFromORq rqty => MsgsFromORq rqty ost orq mins
+      end.
 
-End Reify.
+    Definition interpBindValue {bt} (bv: HBindValue bt): option bt :=
+      match bv with
+      | HGetFirstMsg => getFirstMsg mins
+      end.
 
-Ltac reifyValue val :=
-  match val with
-  | hvec_ith ?ost ?idx => constr:(HOstVal ost idx)
-  | _ => constr:(HConst val)
-  end.
+  End Interp.
 
-Ltac reifyProp p :=
-  match p with
-  | ?p1 /\ ?p2 => let pr1 := reifyProp p1 in
-                  let pr2 := reifyProp p2 in
-                  constr:(HAnd pr1 pr2)
-  | ?p1 \/ ?p2 => let pr1 := reifyProp p1 in
-                  let pr2 := reifyProp p2 in
-                  constr:(HOr pr1 pr2)
-  | ?e1 = ?e2 => let vr1 := reifyValue e1 in
-                 let vr2 := reifyValue e2 in
-                 constr:(HEq vr1 vr2)
-  | ?e1 <> ?e2 => let vr1 := reifyValue e1 in
-                  let vr2 := reifyValue e2 in
-                  constr:(HNe vr1 vr2)
-  | lt ?e1 ?e2 => let vr1 := reifyValue e1 in
-                  let vr2 := reifyValue e2 in
-                  constr:(HLt vr1 vr2)
-  | le ?e1 ?e2 => let vr1 := reifyValue e1 in
-                  let vr2 := reifyValue e2 in
-                  constr:(HLe vr1 vr2)
-  | gt ?e1 ?e2 => let vr1 := reifyValue e1 in
-                  let vr2 := reifyValue e2 in
-                  constr:(HGt vr1 vr2)
-  | ge ?e1 ?e2 => let vr1 := reifyValue e1 in
-                  let vr2 := reifyValue e2 in
-                  constr:(HGe vr1 vr2)
-  end.
+  Fixpoint interpOPrec (p: HOPrec): OPrec :=
+    match p with
+    | HOPrecAnd p1 p2 => interpOPrec p1 /\oprec interpOPrec p2
+    | HOPrecRqRs rp => fun ost orq mins => interpOPrecR ost orq mins rp
+    | HOPrecProp pp => fun ost _ _ => interpOPrecP ost pp
+    end.
 
-Ltac reifyRqRsOPrec oprec :=
-  match oprec with
-  | RqAccepting => constr:(HRqAccepting)
-  | RsAccepting => constr:(HRsAccepting)
-  | UpLockFree => constr:(HUpLockFree)
-  | DownLockFree => constr:(HDownLockFree)
-  | FirstMsg => constr:(HFirstMsg)
-  | UpLockMsgId ?mty ?mid => constr:(HUpLockMsgId mty mid)
-  | UpLockMsg => constr:(HUpLockMsg)
-  | UpLockIdxBack => constr:(HUpLockIdxBack)
-  | UpLockBackNone => constr:(HUpLockBackNone)
-  | DownLockMsgId ?mty ?mid => constr:(HDownLockMsgId mty mid)
-  | DownLockMsg => constr:(HDownLockMsg)
-  | DownLockIdxBack => constr:(HDownLockIdxBack)
-  | MsgsFrom ?froms => constr:(HMsgsFrom froms)
-  | MsgIdsFrom ?msgIds => constr:(HMsgIdsFrom msgIds)
-  | MsgIdFromEach ?msgId => constr:(HMsgIdFromEach msgId)
-  | MsgsFromORq ?rqty => constr:(HMsgsFromORq rqty)
-  end.
+  Fixpoint interpMonad (hm: HMonad) (stm: StateM): option StateM :=
+    match hm with
+    | HBind bv cont =>
+      (msg <-- interpBindValue (msgs stm) bv;
+      interpMonad (cont msg) stm)
+    | HRet rv => Some rv
+    end.
 
-Ltac reifyPropF p :=
-  let ep := eval cbv beta in p in
-  let rp := reifyProp ep in
-  exact rp.
+  Definition interpMTrs (trs: HStateMTrs): StateMTrs :=
+    match trs with
+    | HMTrs hm => interpMonad hm
+    end.
+
+  Definition interpOTrs (trs: HOTrs): OTrs :=
+    match trs with
+    | HTrsMTrs mtrs => TrsMTrs (interpMTrs mtrs)
+    end.
   
-Ltac reifyOPrec oprec :=
-  match oprec with
-  | OPrecAnd ?op1 ?op2 =>
-    let rop1 := reifyOPrec op1 in
-    let rop2 := reifyOPrec op2 in
-    constr:(HOPrecAnd rop1 rop2)
-  | _ =>
-    let rop := reifyRqRsOPrec oprec in
-    constr:(HOPrecT rop)
-  | _ => constr:(HOPrecP (fun ost orq mins =>
-                            ltac:(reifyPropF (oprec ost orq mins))))
-  end.
+End Reify.
 
 Section HemiolaDeep.
   Context `{DecValue} `{OStateIfc}.
 
   Record HRule (sr: Rule) :=
-    { hrule_precond: HOPrec (rule_precond sr);
-      hrule_trs: OTrsR (rule_trs sr)
+    { hrule_precond: HOPrec;
+      hrule_precond_ok:
+        forall ost orq mins,
+          interpOPrec hrule_precond ost orq mins = rule_precond sr ost orq mins;
+      hrule_trs: HOTrs;
+      hrule_trs_ok:
+        forall ost orq mins,
+          interpOTrs hrule_trs ost orq mins = rule_trs sr ost orq mins
     }.
 
   Record HObject :=
@@ -160,30 +183,4 @@ Section HemiolaDeep.
 End HemiolaDeep.
 
 Arguments hvec_ith: simpl never.
-
-(* Require Import Hemiola.Ex.Mesi.Mesi. *)
-(* Existing Instance ImplOStateIfc. *)
-
-(* Example hprec cidx: HOPrec (rule_precond (l1GetSImm cidx)). *)
-(* Proof. *)
-(*   simpl. *)
-(*   match goal with *)
-(*   | |- HOPrec ?oprec => *)
-(*     let rop := reifyOPrec oprec in *)
-(*     exact rop *)
-(*   end. *)
-(* Defined. *)
-
-(* Example hr cidx: HRule (l1GetSImm cidx). *)
-(* Proof. *)
-(*   refine {| hrule_name := ""; hrule_precond := _; hrule_trs := _ |}. *)
-(*   - simpl. *)
-(*     match goal with *)
-(*     | |- HOPrec ?oprec => *)
-(*       let rop := reifyOPrec oprec in *)
-(*       exact rop *)
-(*     end. *)
-(*   - simpl. *)
-(*     repeat (intros; constructor). *)
-(* Defined. *)
 
